b55599e547d278e00dd4961d9b54062f061bf2d7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 12:52:46 -0700

    Add Composite

--- ch4/src/com/learn/HtmlTag.java
+++ ch4/src/com/learn/HtmlTag.java
@@ -3,5 +3,5 @@ package com.learn;
 public abstract class HtmlTag {
     public abstract String getTagName();
     public abstract void setStartTag(String tag);
-
+    public abstract void setEndTag(String tag);
 }
