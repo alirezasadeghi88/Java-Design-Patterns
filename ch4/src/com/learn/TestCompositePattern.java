da6861e7e01328f2e8c8ebae379540da7eed5146
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 14:39:31 -0700

    Add Composite

--- ch4/src/com/learn/TestCompositePattern.java
+++ ch4/src/com/learn/TestCompositePattern.java
@@ -9,5 +9,13 @@ public class TestCompositePattern {
         HtmlTag p1=new HtmlParentElement("<body>");
         p1.setStartTag("<body>");
         p1.setEndTag("</body>");
+
+        parentTag.addChildTag(p1);
+
+        HtmlTag child1=new HtmlElement("<P>");
+        child1.setStartTag("<P>");
+        child1.setEndTag("</P>");
+        child1.setTagBody("Testinghtmltaglibrary");
+        p1.addChildTag(child1);
     }
 }
