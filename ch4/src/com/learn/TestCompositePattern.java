7081f07e40b23d4a4975c4f81b3851dbb4884fb8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 14:40:34 -0700

    Add Composite

--- ch4/src/com/learn/TestCompositePattern.java
+++ ch4/src/com/learn/TestCompositePattern.java
@@ -2,20 +2,28 @@ package com.learn;
 
 public class TestCompositePattern {
     public static void main(String[] args) {
-        HtmlTag parentTag=new HtmlParentElement("<html>");
+        HtmlTag parentTag = new HtmlParentElement("<html>");
         parentTag.setStartTag("<html>");
         parentTag.setEndTag("</html>");
 
-        HtmlTag p1=new HtmlParentElement("<body>");
+        HtmlTag p1 = new HtmlParentElement("<body>");
         p1.setStartTag("<body>");
         p1.setEndTag("</body>");
 
         parentTag.addChildTag(p1);
 
-        HtmlTag child1=new HtmlElement("<P>");
+        HtmlTag child1 = new HtmlElement("<P>");
         child1.setStartTag("<P>");
         child1.setEndTag("</P>");
         child1.setTagBody("Testinghtmltaglibrary");
         p1.addChildTag(child1);
+
+        child1 = new HtmlElement("<P>");
+        child1.setStartTag("<P>");
+        child1.setEndTag("</P>");
+        child1.setTagBody("Paragraph2");
+        p1.addChildTag(child1);
+
+        parentTag.generateHtml();
     }
 }
