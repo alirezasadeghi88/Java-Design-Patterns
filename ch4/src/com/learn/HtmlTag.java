ea5065f17f9ad3fa6a30e7a16ced18b94025e9d5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 12:54:55 -0700

    Add Composite

--- ch4/src/com/learn/HtmlTag.java
+++ ch4/src/com/learn/HtmlTag.java
@@ -4,4 +4,8 @@ public abstract class HtmlTag {
     public abstract String getTagName();
     public abstract void setStartTag(String tag);
     public abstract void setEndTag(String tag);
+
+    public void setTagBody(String tagBody){
+        throw new UnsupportedOperationException("Current operation is not support for this object");
+    }
 }
