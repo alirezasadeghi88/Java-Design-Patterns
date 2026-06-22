ac046d196956505987954cb46e69274026dc3378
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 12:56:05 -0700

    Add Composite

--- ch4/src/com/learn/HtmlTag.java
+++ ch4/src/com/learn/HtmlTag.java
@@ -8,4 +8,8 @@ public abstract class HtmlTag {
     public void setTagBody(String tagBody){
         throw new UnsupportedOperationException("Current operation is not support for this object");
     }
+
+    public void addChildTag(HtmlTag htmlTag) {
+        throw new UnsupportedOperationException("Current operation is not support for this object");
+    }
 }
