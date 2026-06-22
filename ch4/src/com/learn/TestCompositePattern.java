cfec034aeb2a430adfd07019fb4da972ec1e31f1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-22 14:38:19 -0700

    Add Composite

--- ch4/src/com/learn/TestCompositePattern.java
+++ ch4/src/com/learn/TestCompositePattern.java
@@ -5,5 +5,9 @@ public class TestCompositePattern {
         HtmlTag parentTag=new HtmlParentElement("<html>");
         parentTag.setStartTag("<html>");
         parentTag.setEndTag("</html>");
+
+        HtmlTag p1=new HtmlParentElement("<body>");
+        p1.setStartTag("<body>");
+        p1.setEndTag("</body>");
     }
 }
