fa0e85a718089dfb5c70100ab7bffdda70a59f21
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-23 03:36:22 -0700

    Add bridge

--- ch5/src/CentralLocking.java
+++ ch5/src/CentralLocking.java
@@ -1,2 +1,7 @@
 public class CentralLocking implements Product{
+    private final String productName;
+
+    public CentralLocking(String productName) {
+        this.productName = productName;
+    }
 }
