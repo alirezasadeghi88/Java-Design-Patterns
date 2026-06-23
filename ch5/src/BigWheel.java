af147737fc6dffe1c032a1101666762c0c50286b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-23 03:48:03 -0700

    Add BigWheel

--- ch5/src/BigWheel.java
+++ ch5/src/BigWheel.java
@@ -1,2 +1,9 @@
 public class BigWheel extends Car{
+    private final Product product;
+    private final String carType;
+
+    public BigWheel(Product product, String carType) {
+        this.product = product;
+        this.carType = carType;
+    }
 }
