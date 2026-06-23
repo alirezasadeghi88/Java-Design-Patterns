fff82c9d28ecacfa6d8f407332d67c31f0e734e6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-23 03:42:47 -0700

    Add Car

--- ch5/src/Car.java
+++ ch5/src/Car.java
@@ -1,2 +1,9 @@
 public abstract class Car {
+    private final Product product;
+    private final String carType;
+
+    protected Car(Product product, String carType) {
+        this.product = product;
+        this.carType = carType;
+    }
 }
