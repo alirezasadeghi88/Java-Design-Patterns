e33364ad4d40194439b06e49c96812f0dd733128
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-23 04:07:08 -0700

    Add TestBridgePattern

--- ch5/src/TestBridgePattern.java
+++ ch5/src/TestBridgePattern.java
@@ -1,25 +1,27 @@
 public class TestBridgePattern {
     public static void main(String[] args) {
 
-        Product product = new CentralLocking("CentralLockingSystem");
-        Product product1 = new GearLocking("GearLockingSystem");
-        Car car = new BigWheel(product,"BigWheelxzmodel");
+        Product product = new CentralLocking("Central Locking System");
+        Product product1 = new GearLocking("Gear Locking System");
+        Car car = new BigWheel(product,"BigWheel xz model");
         car.produceProduct();
         car.assemble();
         car.printDetails();
 
         System.out.println();
 
-        car = new BigWheel(product1, "BigWheelxzmodel");
+        car = new BigWheel(product1, "BigWheel xz model");
         car.produceProduct();
         car.assemble();
         car.printDetails();
 
-        car = new Motoren(product, "Motorenlmmodel");
+        car = new Motoren(product, "Motoren lm model");
         car.produceProduct();
         car.assemble();
         car.printDetails();
 
         System.out.println();
+
+        car = new Motoren(product1, "Motoren lm model");
     }
 }
