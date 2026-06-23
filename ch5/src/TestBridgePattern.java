feb3fd043b6aa584d2f4bb683684ed294e1e6390
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-23 04:01:19 -0700

    Add TestBridgePattern

--- ch5/src/TestBridgePattern.java
+++ ch5/src/TestBridgePattern.java
@@ -2,5 +2,7 @@ public class TestBridgePattern {
     public static void main(String[] args) {
 
         Product product = new CentralLocking("CentralLockingSystem");
+        Product product1 = new GearLocking("GearLockingSystem");
+
     }
 }
