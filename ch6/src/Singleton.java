05aa67b6c6cf66eddec5291222ed117c7085a790
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-28 01:38:26 -0700

    add class Singleton

--- ch6/src/Singleton.java
+++ ch6/src/Singleton.java
@@ -3,4 +3,10 @@ import java.io.Serializable;
 public class Singleton implements Serializable {
     private static final long serialVersionUID=-1093810940935189395L;
     private static Singleton sc=new Singleton();
+    private Singleton(){
+        if(sc!=null){
+            throw new IllegalStateException("Alreadycreated.");
+        }
+
+    }
 }
