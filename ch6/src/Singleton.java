c3eac49313a1e47974327f04a4a4aa13ad3906a2
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-06-28 01:37:20 -0700

    add class Singleton

--- ch6/src/Singleton.java
+++ ch6/src/Singleton.java
@@ -2,4 +2,5 @@ import java.io.Serializable;
 
 public class Singleton implements Serializable {
     private static final long serialVersionUID=-1093810940935189395L;
+    private static Singleton sc=new Singleton();
 }
