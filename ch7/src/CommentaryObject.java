8f74b5345adcd9f4da99b8f4707242b67c1ef01c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-04 07:30:06 -0700

    Add class CommentaryObject

--- ch7/src/CommentaryObject.java
+++ ch7/src/CommentaryObject.java
@@ -1,6 +1,16 @@
 import interfaces.Commentary;
+import interfaces.Observer;
 import interfaces.Subject;
 
-public class CommentaryObject implements Subject, Commentary {
+import java.util.List;
 
+public class CommentaryObject implements Subject, Commentary {
+    private  final List<Observer> observers;
+    private String desc;
+    private final String subjectDetails;
+
+    public CommentaryObject(List<Observer> observers, String subjectDetails) {
+        this.observers = observers;
+        this.subjectDetails = subjectDetails;
+    }
 }
