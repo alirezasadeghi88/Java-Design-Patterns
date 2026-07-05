b9df0945b37f16500fc436934aba37583f56099c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-05 15:30:55 +0330

    Add class SMSUser

--- ch7/src/SMSUser.java
+++ ch7/src/SMSUser.java
@@ -1,2 +1,8 @@
-public class SMSUser {
+import interfaces.Observer;
+import interfaces.Subject;
+
+public class SMSUser implements Observer {
+    private final Subject subject;
+    private String desc;
+    private String userInfo;
 }
