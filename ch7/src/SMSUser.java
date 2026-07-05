e12bfb92fa8940933bf52dae09881171cf46ca11
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-05 15:32:33 +0330

    Add class SMSUser

--- ch7/src/SMSUser.java
+++ ch7/src/SMSUser.java
@@ -2,7 +2,15 @@ import interfaces.Observer;
 import interfaces.Subject;
 
 public class SMSUser implements Observer {
-    private final Subject subject;
+    private Subject subject;
     private String desc;
     private String userInfo;
+
+    public void SMSUsers(Subject subject, String userInfo){
+        if(subject==null){
+            throw new IllegalArgumentException("NoPublisherfound.");
+        }
+        this.subject=subject;
+        this.userInfo=userInfo;
+    }
 }
