85898733e9ab579c1667489059881334631dac72
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-05 15:38:17 +0330

    Add class TestObserver

--- ch7/src/TestObserver.java
+++ ch7/src/TestObserver.java
@@ -1,2 +1,11 @@
+import interfaces.Observer;
+import interfaces.Subject;
+
+import java.util.ArrayList;
+
 public class TestObserver {
+    public static void main(String[] args) {
+        Subject subject=new CommentaryObject(new ArrayList<Observer>(),
+                "Soccer Match[2014AUG24]");
+    }
 }
