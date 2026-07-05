86dea23881ef8acd05c6353e65faaab5ec6532a7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-05 15:34:42 +0330

    Add class SMSUser

--- ch7/src/SMSUser.java
+++ ch7/src/SMSUser.java
@@ -13,4 +13,30 @@ public class SMSUser implements Observer {
         this.subject=subject;
         this.userInfo=userInfo;
     }
+
+    @Override
+    public void update(String desc) {
+        this.desc=desc;
+        display();
+    }
+
+    private void display() {
+        System.out.println("["+userInfo+"]:"+desc);
+    }
+
+    @Override
+    public void subscribe() {
+        System.out.println("Subscribing"+userInfo+"to"+subject.subjectDetails()+
+                "...");
+        this.subject.subscribeObserver((java.util.Observer) this);
+        System.out.println("Subscribedsuccessfully.");
+    }
+
+    @Override
+    public void unSubscribe() {
+        System.out.println("Unsubscribing"+userInfo+"to"+subject.subjectDetails
+        ()+"...");
+        this.subject.unSubscribeObserver((java.util.Observer) this);
+        System.out.println("Unsubscribedsuccessfully.");
+    }
 }
