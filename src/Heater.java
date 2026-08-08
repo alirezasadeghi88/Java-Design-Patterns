e1546f14de2239b168574f242aec5861871d2bd1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-08 15:03:34 +0330

    Add class Heater

--- src/Heater.java
+++ src/Heater.java
@@ -3,8 +3,17 @@ import interfaces.MachineMediator;
 
 public class Heater implements Colleague {
     private MachineMediator mediator;
+
     @Override
     public void setMediator(MachineMediator mediator) {
         this.mediator = mediator;
+    }
+
+    public void on(int temp) {
+        System.out.println("Heater is on...");
+        if (mediator.checkTemperature(temp)) {
+            System.out.println("Temperature is set to"+temp);
+            mediator.off();
+        }
     }
 }
